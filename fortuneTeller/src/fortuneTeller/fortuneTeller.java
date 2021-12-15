35d886fc1a4bd95493c1ec91d59bfebf4aca0526
corey <corey@192.168.254.23>
2021-12-15 01:32:12 -0500

    update

--- fortuneTeller/src/fortuneTeller/fortuneTeller.java
+++ fortuneTeller/src/fortuneTeller/fortuneTeller.java
@@ -3,6 +3,7 @@
  */
 package fortuneTeller;
 
+import java.util.Scanner;
 /**
  * @author corey
  *
@@ -14,7 +15,18 @@ public class fortuneTeller {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Pick a color");
+		System.out.println(" {1. Red, 2. White, 3. Blue}");
+		Scanner scanner = new Scanner(System.in);
+		
+		int userColor = scanner.nextInt();
+		
+		if (userColor < 1) {
+			System.out.println("Enjoy adventures");
+		} else {
+			System.out.println("Enjoy tranquility");
+		}
 	}
 
 }
